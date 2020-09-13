2ac3a76c78481831a8d4b5a0a92ed73550b02bbb
adifranco2 <70043640+adifranco2@users.noreply.github.com>
2020-09-13 13:24:50 -0400

    initial upload of files

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert("Hello, World"); //using the window.alert method
+
+var A="Testing"
+var B="Concatenate"
+document.write(A + B); //testing cancatenate
+
+var Country="United States", State="Florida", County="Broward", City="Hollywood"; //defining variables and giving them string values
+
+
+
+var Country=Country.fontcolor("purple");  //using the fontcolor method on string variable
+var State=State.fontcolor("blue");
+var County=County.fontcolor("red");
+var City=City.fontcolor("yellow");
+document.write(County); //putting the value of the result into HTML element
+
+document.write(2*4); //using the document.write method
